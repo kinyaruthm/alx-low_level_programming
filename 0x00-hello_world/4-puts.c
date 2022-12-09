21e1059d24c78bac39c6c63d6a91f8f5dd6ec9ec
Kinya <kinyaruthm@gmail.com>
2022-12-09 07:34:37 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ *main - Entry point
  *
- * Return: 0 
+ *Return: 0 
 */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
