b2485773b50417611930e6041cb0d65dabebba2b
Kinya <kinyaruthm@gmail.com>
2022-12-10 12:21:16 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- *
- * main -Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
 */
@@ -16,6 +15,3 @@ int main(void)
 			putchar(c);
 			c--;
 		}
-	putchar('\n');
-	return (0);
-}
