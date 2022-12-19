951b38956485df66344b2ae03769830657e01c3b
Kinya <kinyaruthm@gmail.com>
2022-12-19 00:39:55 -0800

    a function that prints a string in reverse

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: A pointer to an int taht will be changed
+ * Return: Always 0 (success)
+ */
+
+void _puts(char *str)
+{
+	char *c;
+	int _
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - pritns a string in reverse order
+ * @s: A pointer to the string to be changed
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[1] != '\0')
+	{
+	i++;
+	}
+}
