a706768ddc426ba9fdbaee11532788fa5b20d343
Kinya <kinyaruthm@gmail.com>
2022-12-21 02:39:46 -0800

    task 11

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,82 @@
+#include "main.h"
+
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+/**
+ * add_strings - adds the numbers stored in two strings
+ * @n1: The string containing the first number
+ * @n2: The string containing the second number
+ * @r: The buffer to store the results
+ * @r_index: The current index of the buffer
+ * Return: If r canstore sum - a pointer to the result
+ * if R cannot store the sum - 0
+ */
+
+char *add_strings(char *n1, char *n2, char *r, int r_index)
+{
+	int num, tens = 0;
+
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+
+	for (; *n1; n1--, r_index--)
+	{
+		num = (*n1 - '0') + tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
+	
+	for (; *n2; n2--, r_index--)
+        {
+                num = (*n2 - '0') + tens;
+                *(r + r_index) = (num % 10) + '0';
+                tens = num / 10;
+        }
+
+	if (tens && r_index >= 0)
+	{
+		*(r + r_index) = (tens % 10) + '0';
+		return (r + r_index);
+	}
+
+	else if (tens && r_index < 0)
+		return (0);
+	return (r + r_index + 1);
+}
+
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer to store results
+ * @size_r: buffer size
+ * return: if r can store - a pointer to the results
+ *  if r cannot store - 0
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int index, n1_len = 0, n2_len = 0;
+
+	for (index = 0; *(n1 + index); index++)
+		n1_len++;
+	for (index = 0; *(n2 + index); index++)
+                n2_len++;
+	
+	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
+		return (0);
+	n1 += n1_len - 1;
+	n2 += n2_len - 1;
+	*(r + size_r) = '\0';
+
+	return (add_strings(n1, n2, r, --size_r));
+}
+
+ 
+
+ 
