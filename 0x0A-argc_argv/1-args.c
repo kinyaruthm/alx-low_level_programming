d511000c3f2a81013b6a8e1d74d923616feff857
Kinya <kinyaruthm@gmail.com>
2023-01-09 08:19:28 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: count arguments
+ * @argv: Argument
+ * Return: Always 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%i\n", argc - 1);
+
+return (0);
+}
+
