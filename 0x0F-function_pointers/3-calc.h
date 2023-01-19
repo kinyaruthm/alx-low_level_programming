a124dfc016fc251c5425ca29d492576c762e086c
Kinya <kinyaruthm@gmail.com>
2023-01-19 03:36:49 -0800

    task 3

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct op - Struct op
  *
@@ -9,3 +13,10 @@ typedef struct op
     char *op;
     int (*f)(int a, int b);
 } op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculates the sum of the integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * op_sub - calculates the sum of the integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+
+/**
+ * op_mul - calculates the multiplication of the integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+
+/**
+ * op_div - calculates the division of the integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+
+/**
+ * op_mod - calculates the remainder of division of the integers
+ * @a: first integer
+ * @b: second integer
+ * Return: sum of a and b
+ */
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
+
