abb5f06b9bf42a3e901c16f0da7879ce63e9b757
Kinya <kinyaruthm@gmail.com>
2023-01-30 20:43:58 -0800

    task 6

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,7 +8,7 @@ void _attribute_((constructor)) hare(void);
 
 void hare(void)
 {
-	printf("You're beat! and yet, you must allow,"
-		"\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
 
