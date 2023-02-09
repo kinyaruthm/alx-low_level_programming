424f77fd0d01e938470d46eded1f9b6080d4cbf7
Kinya <kinyaruthm@gmail.com>
2023-02-09 10:43:36 -0800

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0000);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0000);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
