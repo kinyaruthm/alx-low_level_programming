bbecfea9a4a3bfabf66e9e212d9eb5133bf766f6
Kinya <kinyaruthm@gmail.com>
2023-02-09 10:31:17 -0800

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,11 +20,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 
-	o = open(filename, 0_RDONLY);
+	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO< buffer, r);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	if (o == -1 || r == -1 || w != r)
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
 		free(buffer);
 		return (0);
